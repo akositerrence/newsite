bcd29d17027a2004a3633cf1c19d5ce5875da8c0
akositerrence <terrsangabriel@gmail.com>
2024-07-25 14:37:11 -0500

    minor fixes

---
+++ javascript/nav.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function() {
+    const page = this.document.querySelector("page");
+    const menu = this.document.querySelector("#menu-button");
+    console.log(page);
+})
