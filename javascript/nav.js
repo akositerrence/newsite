4f4ec2d4c1b3143f1c6a4cd0a68ae9fb9d5bcf9f
akositerrence <terrsangabriel@gmail.com>
2024-07-26 00:10:12 -0500

    initial staging

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,10 +1,28 @@
 window.addEventListener("load", function() {
     const menu = document.querySelector("#mobile-menu-button");
     const page = Array.from(document.querySelectorAll(".page"));
+    const title = document.querySelector("#mobile-title");
+    const logo = document.querySelector("#mobile-logo");
 
     menu.addEventListener("click", function() {
         page.forEach((p, index) => {
             p.style.transform = "translateX(-50vw)";
         })
     });
+
+    this.window.addEventListener("scroll", function() {
+        const scrollPosition = window.scrollY;
+        const treshold = window.innerWidth * 0.25;
+        if( scrollPosition > treshold ) {
+            title.style.transform = "translateY(-15vw)";
+            menu.style.transform = "translateX(5.5vw)";
+            logo.style.transform = "translateX(-5.5vw)";
+        } else {
+            title.style.transform = "translateY(0)";
+            menu.style.transform = "translateX(0)";
+            logo.style.transform = "translateX(0)";
+        }
+
+    });
+
 })
