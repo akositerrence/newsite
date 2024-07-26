72807a8095efca15131aa9372fbef88a57f4662d
akositerrence <terrsangabriel@gmail.com>
2024-07-26 00:19:26 -0500

    minor js performance fix

--- javascript/nav.js
+++ javascript/nav.js
@@ -4,25 +4,24 @@ window.addEventListener("load", function() {
     const title = document.querySelector("#mobile-title");
     const logo = document.querySelector("#mobile-logo");
 
-    menu.addEventListener("click", function() {
-        page.forEach((p, index) => {
-            p.style.transform = "translateX(-50vw)";
-        })
-    });
-
-    this.window.addEventListener("scroll", function() {
-        const scrollPosition = window.scrollY;
-        const treshold = window.innerWidth * 0.25;
-        if( scrollPosition > treshold ) {
-            title.style.transform = "translateY(-15vw)";
-            menu.style.transform = "translateX(5.5vw)";
-            logo.style.transform = "translateX(-5.5vw)";
-        } else {
-            title.style.transform = "translateY(0)";
-            menu.style.transform = "translateX(0)";
-            logo.style.transform = "translateX(0)";
-        }
-
-    });
-
+    if(this.window.innerWidth <= 768) {
+        menu.addEventListener("click", function() {
+            page.forEach((p, index) => {
+                p.style.transform = "translateX(-50vw)";
+            })
+        });
+        this.window.addEventListener("scroll", function() {
+            const scrollPosition = window.scrollY;
+            const treshold = window.innerWidth * 0.25;
+            if(scrollPosition > treshold) {
+                title.style.transform = "translateY(-15vw)";
+                menu.style.transform = "translateX(5.5vw)";
+                logo.style.transform = "translateX(-5.5vw)";
+            } else {
+                title.style.transform = "translateY(0)";
+                menu.style.transform = "translateX(0)";
+                logo.style.transform = "translateX(0)";
+            }
+        });
+    }
 })
