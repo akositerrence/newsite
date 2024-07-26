e323057207616d11ee69dec7a89dfe71949b41a6
akositerrence <terrsangabriel@gmail.com>
2024-07-25 22:08:10 -0500

    started nav js

--- javascript/mobilenav.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener("load", function() {
-    // do title disappear - experiment w css transition instead of based on scroll
-    // make menu icons go to the side. if exceed certain # of pixels, then use transform. applyy css transition
-})
--- javascript/nav.js
+++ javascript/nav.js
@@ -1,5 +1,10 @@
 window.addEventListener("load", function() {
-    const page = this.document.querySelector("page");
-    const menu = this.document.querySelector("#menu-button");
-    console.log(page);
+    const menu = document.querySelector("#mobile-menu-button");
+    const page = Array.from(document.querySelectorAll(".page"));
+
+    menu.addEventListener("click", function() {
+        page.forEach((p, index) => {
+            p.style.transform = "translateX(-50vw)";
+        })
+    });
 })
